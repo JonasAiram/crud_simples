f1ac6fe2aee070bc8a9cc8af5e24f58f05a5a22e
Jonas Airam <jonas.airam@orubetecnologia.com.br>
2020-09-29 10:07:20 -0300

    ajuste user mysql e add model motorista

---
+++ app/Models/Models/Motorista.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Motorista extends Model
+{
+    use HasFactory;
+
+    protected $table = 'motorista';
+
+}
